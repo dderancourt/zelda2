c2fd10e27f0ea0c59162f50fe7b851b19e1c0ff3
david derancourt <david.derancourt@hotmail.fr>
2020-01-29 11:55:31 +0100

    display of the two auth form, a bit of design on signup and login

--- frontend/src/Components/Utilities/Login.jsx
+++ frontend/src/Components/Utilities/Login.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Style/LoginSignup.scss";
 
 export default function Login() {
-  return <div>Login work</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sublogin = e => {};
+
+  return (
+    <div className="form">
+      <form
+        onSubmit={e => {
+          sublogin(e);
+        }}
+      >
+        <label className="button">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            placeholder="email"
+            onChange={evt => setEmail(evt.target.value)}
+          ></input>
+        </label>
+        <label className="button">
+          <input
+            id="password"
+            name="password"
+            placeholder="mot de passe"
+            type="password"
+            value={password}
+            onChange={evt => setPassword(evt.target.value)}
+          ></input>
+        </label>
+        <input className="button" type="submit" value="Soummettre"></input>
+      </form>
+    </div>
+  );
 }
--- frontend/src/Components/Utilities/Signup.jsx
+++ frontend/src/Components/Utilities/Signup.jsx
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Style/LoginSignup.scss";
+import axios from "axios";
 
 export default function SignUp() {
-  return <div>SignUp work</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const check = () => {};
+
+  function signup(e) {}
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          signup(e);
+        }}
+      >
+        <label className="button">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            placeholder="email"
+            onChange={evt => setEmail(evt.target.value)}
+          ></input>
+        </label>
+        <label className="button">
+          <input
+            id="password"
+            name="password"
+            placeholder="mot de passe"
+            type="password"
+            value={password}
+            onChange={evt => setPassword(evt.target.value)}
+          ></input>
+          <p className={check() ? "wrong" : "valid"}>
+            {password.length < 8 ? "8 caractères minimum" : ""}
+          </p>
+        </label>
+
+        <label className="button">
+          <input
+            id="confirmPassword"
+            name="confirmPassword"
+            placeholder="mot de passe"
+            type="password"
+            value={confirmPassword}
+            onChange={evt => setConfirmPassword(evt.target.value)}
+          ></input>
+          <p className={check() ? "wrong" : "valid"}>
+            {check() ? "" : "les 2 mots de passe doivent être identiques."}
+          </p>
+        </label>
+        <input
+          className="button"
+          type="submit"
+          value="Soummettre"
+          onClick={() => {
+            check();
+          }}
+        ></input>
+      </form>
+    </div>
+  );
 }
