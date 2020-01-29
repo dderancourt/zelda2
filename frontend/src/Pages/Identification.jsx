a454f1d21c05421f92c23f2787b685f1e941efc5
david derancourt <david.derancourt@hotmail.fr>
2020-01-29 11:19:33 +0100

    positionning of main elements on identification

--- frontend/src/Pages/Identification.jsx
+++ frontend/src/Pages/Identification.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import Login from "../Components/Utilities/Login";
 import SingUp from "../Components/Utilities/Signup";
+import "./Style/Identification.scss";
 
 function Identification() {
   const [createCount, setCreateCount] = useState(true);
 
   return (
-    <div>
-      <div>{createCount ? <Login /> : <SingUp />}</div>
-      <p>no account ?</p>
-      <button onClick={() => setCreateCount(!createCount)}>
-        create an account
-      </button>
+    <div className="Page">
+      <div className="identificationCard">
+        <div>{createCount ? <Login /> : <SingUp />}</div>
+        <p>no account ?</p>
+        <button onClick={() => setCreateCount(!createCount)}>
+          create an account
+        </button>
+      </div>
     </div>
   );
 }
