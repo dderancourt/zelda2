67b2926b8c849883987e69d9cc1e8845425cffb6
david derancourt <david.derancourt@hotmail.fr>
2020-01-29 10:19:30 +0100

    creation of reducer and store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/game" component={Game} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./reset.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
